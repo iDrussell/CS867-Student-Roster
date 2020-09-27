d51fa49e0ab13954b08dd85dbe10e3f0bf07e94e
iDrussell <dalton.c.russell@protonmail.com>
2020-09-27 14:51:26 -0400

    Fixed SecurityStudent class to match base class.

--- securityStudent.cpp
+++ securityStudent.cpp
@@ -1,29 +1,32 @@
 #include "securityStudent.h"
 #include <iostream>
-using namespace std;
 
-
+namespace StudentRoster {
 //constructor
-SecurityStudent::SecurityStudent() : Student() {}
+    SecurityStudent::SecurityStudent()
+            :Student() { }
 
 // Returns  degree program
-Degree SecurityStudent::GetDegreeProgram() const {
-  return this->degreeProgram;
-}
+    Degree SecurityStudent::GetDegreeProgram() const
+    {
+        return degree_program_;
+    }
 
-// Printst student information
-void SecurityStudent::Print() const {
-  cout << Student::GetStudentID() << "\t";
-  cout << "First Name: " << Student::GetFirstName() <<" \t";
-  cout << "Last Name: " << Student::GetLastName() << " \t";
-  cout << "Age: " << Student::GetAge() << " \t";
-  const int* days;
-  days = Student::GetNumDays();
-  cout << "daysInCourse: {" << days[0] << ", " << days[1] << ", " << days[2] << "} ";
-  cout << "Degree Program: Security" << endl;
-}
+// Print student information
+    void SecurityStudent::Print() const
+    {
+        std::cout << Student::GetStudentID() << "\t";
+        std::cout << "First Name: " << Student::GetFirstName() << " \t";
+        std::cout << "Last Name: " << Student::GetLastName() << " \t";
+        std::cout << "Age: " << Student::GetAge() << " \t";
+        const std::array<int, 3> days = Student::GetNumDays();
+        std::cout << "daysInCourse: {" << days[0] << ", " << days[1] << ", " << days[2] << "} ";
+        std::cout << "Degree Program: Security\n";
+    }
 
 // Sets student's degree program
-void SecurityStudent::SetDegreeProgram(Degree degreeProgram) {
-  this->degreeProgram = degreeProgram;
-}
+    void SecurityStudent::SetDegreeProgram(Degree degree_program)
+    {
+        degree_program_ = degree_program;
+    }
+} // namespace StudentRoster
--- securityStudent.h
+++ securityStudent.h
@@ -2,17 +2,16 @@
 #define SECURITYSTUDENT_H
 #include "student.h"
 #include "degree.h"
-using namespace std;
 
-class SecurityStudent: public Student {
-  public:
-
-    Degree GetDegreeProgram() const;
-    void Print() const;
-    SecurityStudent();
-    void SetDegreeProgram(Degree degreeProgram);
-  private:
-    Degree degreeProgram;
-
-};
+namespace StudentRoster {
+    class SecurityStudent : public Student {
+    public:
+        Degree GetDegreeProgram() const;
+        void Print() const;
+        SecurityStudent();
+        void SetDegreeProgram(Degree degree_program);
+    private:
+        Degree degree_program_;
+    };
+} // namespace StudentRoster
 #endif
